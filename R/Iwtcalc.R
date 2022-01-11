fe1e1db5099be88dbf97de24f553d43a72da93c1
Grayson White <graysonwhite13@gmail.com>
2022-01-11 14:52:13 -0800

    remove internal functions -- moved to `graysonwhite/FIESTAutils`

--- R/Iwtcalc.R
+++
@@ -1,8 +0,0 @@
-wtcalc <- function(wtLUT, strvar, acrevar){
-  ## DESCRIPTION: Internal function to calculate weights (proportions) by strata.
-
-  strnames <- unique(wtLUT[,strvar])
-  stracres <- tapply(wtLUT[,acrevar], wtLUT[,strvar], sum)
-  stracres[!(levels(strnames) %in% strnames)] <- 0
-  stracres/sum(stracres)
-  }
