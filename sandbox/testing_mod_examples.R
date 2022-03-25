5df70043267cf1debf2f6a5ca389be9603f51944
Grayson White <graysonwhite13@gmail.com>
2022-03-25 15:08:42 -0600

    work on SApopdat

--- sandbox/testing_mod_examples.R
+++ sandbox/testing_mod_examples.R
@@ -15,3 +15,18 @@ MApopdat2 <- modMApop(popTabs = list(tree = FIESTA::WYtree,
 
 test <- modMAarea(MApopdat = MApopdat2,
           MAmethod = "greg")
+
+SApopdat <- modSApop(popTabs = list(tree = FIESTA::WYtree,
+                                    cond = FIESTA::WYcond),
+                     pltassgn = FIESTA::WYpltassgn,
+                     pltassgnid = "CN",
+                     dunitarea = FIESTA::WYunitarea,
+                     dunitvar = "ESTN_UNIT",
+                     dunitzonal = FIESTA::WYunitzonal,
+                     prednames = c("dem", "tcc", "tpi"),
+                     predfac = "tnt")
+
+SApopdat <- modSApop(pltdat = SApltdat, 
+                     auxdat = auxdat,
+                     smallbnd = WYbhdistfn,
+                     smallbnd.domain = smallbnd.domain)
