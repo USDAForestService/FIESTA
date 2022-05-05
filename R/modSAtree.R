18920b9af81caf098176725540b214c9d60d0a9a
Grayson White <graysonwhite13@gmail.com>
2022-05-05 09:06:53 -0700

    updates

--- R/modSAtree.R
+++ R/modSAtree.R
@@ -284,8 +284,15 @@ modSAtree <- function(SApopdatlst = NULL,
   
   ## Set user-supplied savedata values
   if (length(savedata_opts) > 0) {
+    if (!savedata) {
+      message("savedata=FALSE with savedata parameters... no data are saved")
+    }
     for (i in 1:length(savedata_opts)) {
-      assign(names(savedata_opts)[[i]], savedata_opts[[i]])
+      if (names(savedata_opts)[[i]] %in% names(savedata_defaults_list)) {
+        assign(names(savedata_opts)[[i]], savedata_opts[[i]])
+      } else {
+        stop(paste("Invalid parameter: ", names(savedata_opts)[[i]]))
+      }
     }
   }
   
@@ -299,7 +306,11 @@ modSAtree <- function(SApopdatlst = NULL,
   ## Set user-supplied table values
   if (length(table_opts) > 0) {
     for (i in 1:length(table_opts)) {
-      assign(names(table_opts)[[i]], table_opts[[i]])
+      if (names(table_opts)[[i]] %in% names(table_defaults_list)) {
+        assign(names(table_opts)[[i]], table_opts[[i]])
+      } else {
+        stop(paste("Invalid parameter: ", names(table_opts)[[i]]))
+      }
     }
   }
   
@@ -313,7 +324,11 @@ modSAtree <- function(SApopdatlst = NULL,
   ## Set user-supplied title values
   if (length(title_opts) > 0) {
     for (i in 1:length(title_opts)) {
-      assign(names(title_opts)[[i]], title_opts[[i]])
+      if (names(title_opts)[[i]] %in% names(title_defaults_list)) {
+        assign(names(title_opts)[[i]], title_opts[[i]])
+      } else {
+        stop(paste("Invalid parameter: ", names(title_opts)[[i]]))
+      }
     }
   }
   
@@ -327,7 +342,11 @@ modSAtree <- function(SApopdatlst = NULL,
   ## Set user-supplied multest values
   if (length(multest_opts) > 0) {
     for (i in 1:length(multest_opts)) {
-      assign(names(multest_opts)[[i]], multest_opts[[i]])
+      if (names(multest_opts)[[i]] %in% names(multest_defaults_list)) {
+        assign(names(multest_opts)[[i]], multest_opts[[i]])
+      } else {
+        stop(paste("Invalid parameter: ", names(multest_opts)[[i]]))
+      }
     }
   }
 
@@ -371,7 +390,7 @@ modSAtree <- function(SApopdatlst = NULL,
       SApopdatlst <- list(SApopdatlst)
     }  
 
-    if (class(SApopdatlst) == "list") {
+    if (inherits(SApopdatlst, "list")) {
       list.items <- c("condx", "pltcondx", "treex", "cuniqueid", "condid", 
 		"tuniqueid", "ACI.filter", "dunitarea", "dunitvar", "dunitlut",
 		"prednames", "plotsampcnt", "condsampcnt")
