c5b25f68b3c946747b6941d7b46a621bd5ce5bed
Tracey Frescino <tracey.frescino@usda.gov>
2022-07-29 13:04:56 -0600

    Update ISAinternal.R

--- R/ISAinternal.R
+++ R/ISAinternal.R
@@ -159,7 +159,7 @@ helper.select <- function(smallbndx, smallbnd.unique, smallbnd.domain=NULL,
     maxbnd_max <- merge(maxbndx_intersect, maxbnd_max)
     maxbndxlst <- maxbndxlst[maxbndxlst %in% unique(maxbnd_max[[maxbnd.unique]])]
  
-    if (length(maxbnd.gtthres) > 1) {
+    if (length(maxbnd.gtthres) > 1 || (nrow(maxbnd_max) > 1 && byeach)) {
       message("smallbnd intersects more than 1 maxbnd")
 
       if (multiSAdoms) {
