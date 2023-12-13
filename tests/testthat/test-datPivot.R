8d88ea118c587f86921986fb550dfe1a0086002e
aweisusda <147875668+aweisusda@users.noreply.github.com>
2023-12-13 09:29:22 -0600

    update DatPivot Method

--- tests/testthat/test-datPivot.R
+++ tests/testthat/test-datPivot.R
@@ -8,21 +8,23 @@ test_that("datPivot Testing", {
                     yvar = "TREECLCD", ##Tree Class Code
                     pfun = mean) ##Name of function to use for pivot values
 
+
   # Using datPivot - from Tracy's Example
   test2 <- datPivot(x = WYtree,
-                   pvar = "VOLCFNET",
-                   xvar = "PLT_CN",
-                   yvar = "SPCD",
-                   xfilter = "STATUSCD == 1")
+                    pvar = "VOLCFNET",
+                    xvar = "PLT_CN",
+                    yvar = "SPCD",
+                    xfilter = "STATUSCD == 1")
 
   CN <- 40404730010690
-  test2_subset <- round(test2[test2$PLT_CN == CN, "113",][[1]], 2)
+  test2_subset <- test2[test2$PLT_CN == CN, "113",][[1]]
+  test2_subset_char <- as.character(test2_subset)
 
   input1 <- WYtree[WYtree$PLT_CN == CN, ]
-  input2 <- round(sum(input1[input1$SPCD == 113 & input1$STATUSCD == 1, "VOLCFNET"]), 2)
+  input2 <- sum(input1[input1$SPCD == 113 & input1$STATUSCD == 1, "VOLCFNET"])
+  input3_final <- formatC(input2)
 
-  expect_equal(test2_subset, input2) ##expecting Tracys example code aligns with WYtree data
-  expect_equal(dim(test1), c(10,4)) ##testing dimensions of output
-  expect_snapshot(test1) ##Capturing snapshot
-  expect_snapshot(test2)
+  expect_equal(test2_subset_char, input3_final)
+  expect_equal(dim(test1), c(10,4))
+  expect_snapshot(test1)
 })
